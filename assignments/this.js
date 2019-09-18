4307c6041c4edb8565486f680b701d54185a3571
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-09-18 13:04:12 -0700

    finished the THISjs assignment

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When 'this' is used in a global scope, the value will equal the window or console object. 
+* 2. Whatever is before the dot is 'this' when a function is called.
+* 3. When a constructor function is used, it points to the object being created and returned by the constructor function. 
+* 4. Whenever the 'call' or 'apply' method is being used, this is being defined. 
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,54 @@
 
 // code example for Window Binding
 
+function myName (name) {
+    console.log(this);
+    return name;
+}
+myName("Jose");
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const myCat = {
+    sound: 'meow',
+    catSpeaks: function (name) {
+        console.log( `My name is ${name} and i go ${this.sound}!`);
+    }
+};
+myCat.catSpeaks('Milo');
 
 // Principle 3
 
 // code example for New Binding
 
+function animal(greeter) {
+    this.greeting = 'I go  ';
+    this.greeter = greeter;
+    this.speak = function () {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
+
+const dog = new animal('Woof Woof!');
+const cat = new animal('Meow Meow!');
+
+dog.speak();
+cat.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+function cat() {
+    console.log(this.sound);
+}
+
+let myCat = {
+    name:'Milo',
+    sound: 'Meow Meow'
+}
+
+cat.call(myCat);
